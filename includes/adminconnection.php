db19ca79128037e907e55033ffd257e4cf69b02e
Thomas <goodnes@epubc.site>
2025-04-11 00:52:38 +1200

    Modified  to use root user

--- includes/adminconnection.php
+++ includes/adminconnection.php
@@ -1,6 +1,8 @@
 <?php
 	/*  Connect to the database with the admin user */
-	$dbc = mysqli_connect("localhost", "yangth_Admin", 'Ks>z%rjf7<*%4eC!', "yangth_AssessmentDb");
+	/*$dbc = mysqli_connect("localhost", "yangth_Admin", 'Ks>z%rjf7<*%4eC!', "yangth_AssessmentDb");*/
+	/* Temporarily have a default root connection to simplify process for people who want to play around with the code */
+	$dbc = mysqli_connect("localhost", "root", '', "yangth_AssessmentDb");
 	if (!$dbc){
 		echo "<h1>Error!</h1>";
 		exit();
--- includes/memberconnection.php
+++ includes/memberconnection.php
@@ -1,6 +1,8 @@
 <?php
 	/* Connect to database using a member connection */
-	$dbc = mysqli_connect("localhost", "yangth_Member", 'pkA,#P!taH63;jMN', "yangth_AssessmentDb");
+	/*$dbc = mysqli_connect("localhost", "yangth_Member", 'pkA,#P!taH63;jMN', "yangth_AssessmentDb");*/
+	/* Temporarily have a default root connection to simplify process for people who want to play around with the code */
+	$dbc = mysqli_connect("localhost", "root", '', "yangth_AssessmentDb");
 	if (!$dbc){
 		echo "<h1>Error!</h1>";
 		exit();
